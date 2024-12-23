bb3854050e9a05dc483e8ec2ddb2ad1156908e1a
rakshit-singh2 <thisisjames007@gmail.com>
2024-12-23 15:32:56 +0530

    correction

--- src/Components/Navgation/Navgation.jsx
+++ src/Components/Navgation/Navgation.jsx
@@ -59,7 +59,7 @@ const Navigation = () => {
           {Object.keys(clients).map((key, index) => (
             <li className="nav-item" key={index}>
               <Link className="nav-link d-flex gap-2 align-items-center" to={`/${Object.keys(clients[key].graph)[0]}-${key}`}>
-                <img src={clients[key]?.icon} alt="Ethereum logo" style={{ width: "22px", height: "22px" }} />
+                <img src={clients[key]?.icon} alt="Ethereum logo" style={{ width: "22px", height: "22px", borderRadius: "50%" }} />
                 <span className="fw-semibold">{key}</span>
               </Link>
             </li>
